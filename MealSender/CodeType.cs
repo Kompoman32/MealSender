f8186128a734aa0ec88f4a36bb444ae60eeed4b7
Lipin Slava <Kompoman32@yandex.ru>
2019-12-12 22:36:51 +0500

    something changed

---
+++ MealSender/CodeType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealSender
+{
+    /// <summary>
+    /// Цель сообщения
+    /// </summary>
+    public enum CodeType
+    {
+        /// <summary>
+        /// запускаем волну, для сбора данных о нагрузке
+        /// </summary>
+        waveCheck,
+
+        /// <summary>
+        /// отправляем сообщение по адресу из Info
+        /// </summary>
+        sendMsgTo
+    }
+}
